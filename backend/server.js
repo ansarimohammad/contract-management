5820b02a914956190b4e345929367092694a96f5
Mohammad Ahmad Ansari <ansari101mohammad@gmail.com>
2026-01-20 14:11:18 +0530

    Testing Express working or not

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
 import express from 'express'
+import cors from 'cors'
+
+
+
+const app = express()
+const PORT = 3000
+
+app.use(cors({
+  origin: "http://localhost:5174",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server Running on Port ${PORT}`)
+})
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleBackend = () => {
+    axios.get('/')
+      .then(response => {
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
   return (
     <>
       <div>
-        Home page 
+        Home page
+        <br />
+        <button onClick={handleBackend}>
+          Click to Check Backend
+        </button>
       </div>
     </>
   )
